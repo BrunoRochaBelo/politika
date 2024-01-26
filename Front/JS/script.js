18a27deb8535a7190ddbfc7989b0ec4599e41f4b
BrunoRochaBelo <bruno.r.belo@hotmail.com>
2024-01-26 16:21:02 -0300

    Adição dos icones na barra de navegação do modo responsivo e outros ajustes.

--- Front/JS/script.js
+++ Front/JS/script.js
@@ -1,4 +1,9 @@
-function toggleSidenav() {
+function toggleHomeSidenav() {
   var sidenav = document.querySelector(".home-sidenav");
+  sidenav.classList.toggle("recuar");
+}
+
+function toggleEventosSidenav() {
+  var sidenav = document.querySelector(".eventos-sidenav");
   sidenav.classList.toggle("recuar");
 }
