220b10a2e71c670fb40e83edcbaeaabc79420b7e
BrunoRochaBelo <bruno.r.belo@hotmail.com>
2024-01-29 00:32:36 -0300

    Refatoração geral.

---
+++ Front/JS/RecuarExpandir.js
@@ -0,0 +1,4 @@
+function toggleRecuarExpandir() {
+  var sidenav = document.querySelector(".recuara-expandir");
+  sidenav.classList.toggle("recuar");
+}
--- Front/JS/script.js
+++
@@ -1,9 +0,0 @@
-function toggleHomeSidenav() {
-  var sidenav = document.querySelector(".home-sidenav");
-  sidenav.classList.toggle("recuar");
-}
-
-function toggleEventosSidenav() {
-  var sidenav = document.querySelector(".eventos-sidenav");
-  sidenav.classList.toggle("recuar");
-}
