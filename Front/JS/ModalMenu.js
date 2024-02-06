246adbde460028191ceb25ba59c1e8e26365280a
BrunoRochaBelo <bruno.r.belo@hotmail.com>
2024-02-06 02:24:12 -0300

    Reformulação do modal menu - P1

--- Front/JS/ModalMenu.js
+++ Front/JS/ModalMenu.js
@@ -2,17 +2,17 @@
 function showModal() {
   var modal = document.getElementById("userModal");
   var userPhoto = document.getElementById("userPhoto");
-  var username = "Nome do Usuário"; // Substitua pelo nome real do usuário
+  var username = "Nome do Usuário";
 
   // Define o nome do usuário no modal
-  document.getElementById("username").innerText = username;
+  document.getElementById("candidato").innerText = username;
 
   // Define a foto do usuário no modal
   document.getElementById("modalUserPhoto").src = userPhoto.src;
 
   modal.style.display = "flex";
 
-  // Adiciona um event listener para fechar o modal ao clicar fora do modal-content
+  // Event listener para fechar o modal ao clicar fora do modal-content
   document.addEventListener("click", outsideClickListener);
 }
 
@@ -25,22 +25,24 @@ function hideModal() {
   document.removeEventListener("click", outsideClickListener);
 }
 
-// Função para simular o logout (você pode substituir com seu código real de logout)
+// Função para o logout
 function logout() {
   // Adicione aqui o código real para efetuar o logout
   alert("Logout realizado com sucesso!");
 
-  // Redireciona para a página desejada após o logout
   window.location.href = "./login.html";
 }
 
 // Função para fechar o modal ao clicar fora do modal-content
 function outsideClickListener(event) {
-  var modalContent = document.querySelector(".modal-content");
+  var modalContent = document.querySelector(".modal-content-template");
   var userPhoto = document.getElementById("userPhoto");
 
   // Verifica se o clique ocorreu fora do modal-content e do ícone de usuário
   if (!modalContent.contains(event.target) && event.target !== userPhoto) {
     hideModal();
   }
+
+  // Impede que o evento seja propagado para elementos internos ao modal-content
+  event.stopPropagation();
 }
