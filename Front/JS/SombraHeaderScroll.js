415eb14335777a90e28e1ef970eb7280deadb7a3
BrunoRochaBelo <bruno.r.belo@hotmail.com>
2024-02-12 02:17:35 -0300

    Continuação da alteração do design

--- Front/JS/SombraHeaderScroll.js
+++ Front/JS/SombraHeaderScroll.js
@@ -1,15 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
   var headerTemplate = document.querySelector(".header-template");
+  var headerTitle = document.querySelector(".header-title");
 
   function toggleBoxShadow() {
     var scrollTop = window.scrollY || document.documentElement.scrollTop;
 
     if (scrollTop > 0) {
       headerTemplate.classList.add("box-shadow-active");
+      headerTitle.style.transition = "color 0.3s ease";
+      headerTitle.style.color = "var(--cor-12)";
     } else {
       headerTemplate.classList.remove("box-shadow-active");
+      headerTitle.style.transition = "color 0.3s ease";
+      headerTitle.style.color = "var(--cor-0)";
     }
   }
+
+  // Chama função toggleBoxShadow no carregamento inicial da página
+  toggleBoxShadow();
 
   window.addEventListener("scroll", function () {
     toggleBoxShadow();
