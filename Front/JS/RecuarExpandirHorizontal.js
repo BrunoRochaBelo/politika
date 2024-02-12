5725c808d4681cb3b9646dce2d40c13483a6b180
BrunoRochaBelo <bruno.r.belo@hotmail.com>
2024-02-12 15:34:17 -0300

    Commit de segurança

--- Front/JS/RecuarExpandirHorizontal.js
+++ Front/JS/RecuarExpandirHorizontal.js
@@ -1,4 +1,15 @@
 function toggleRecuarExpandirHorizontal() {
-  var sidenav = document.querySelector(".recuar-expandir-horizontal");
-  sidenav.classList.toggle("recuar");
+  var elemento = document.querySelector(".recuar-expandir-horizontal");
+
+  // Verifica se a classe atual é .lista-horizontal-template
+  if (elemento.classList.contains("lista-horizontal-template")) {
+    // Remove a classe atual
+    elemento.classList.remove("lista-horizontal-template");
+    // Adiciona a nova classe
+    elemento.classList.add("lista-vertical-3-template");
+  } else {
+    // Se a classe atual não for .lista-horizontal-template, inverte a lógica
+    elemento.classList.remove("lista-vertical-3-template");
+    elemento.classList.add("lista-horizontal-template");
+  }
 }
