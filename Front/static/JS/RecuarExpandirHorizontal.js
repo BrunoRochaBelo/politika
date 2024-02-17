a949d53d0053751358815c210acb941fefaaa8e3
BrunoRochaBelo <bruno.r.belo@hotmail.com>
2024-02-16 21:54:47 -0300

    Início da construção do módulo Calendário

--- Front/static/JS/RecuarExpandirHorizontal.js
+++ Front/static/JS/RecuarExpandirHorizontal.js
@@ -1,16 +1,4 @@
 function toggleRecuarExpandirHorizontal() {
-  var elemento = document.querySelector(".recuar-expandir-horizontal");
-
-  // Verifica se a classe atual é .lista-horizontal-template
-  if (elemento.classList.contains("lista-horizontal-template")) {
-    // Remove a classe atual
-    elemento.classList.remove("lista-horizontal-template");
-    // Adiciona a nova classe
-    elemento.classList.add("lista-vertical-3-template");
-  } else {
-    // Se a classe atual não for .lista-horizontal-template, inverte a lógica
-    elemento.classList.remove("lista-vertical-3-template");
-    elemento.classList.add("lista-horizontal-template");
-  }
-  window.scrollTo(0, 0);
+  var element = document.querySelector(".lista-vertical-horizontal");
+  element.classList.toggle("desativar-responsivo");
 }
