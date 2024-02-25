60696f45983727a8274bf149fed3ba86b86dcb71
BrunoRochaBelo <bruno.r.belo@hotmail.com>
2024-02-25 00:33:37 -0300

    Atualizações de estilo para contatos, cabeçalho da página inicial e modais.

--- Front/static/JS/OcultarExibir.js
+++ Front/static/JS/OcultarExibir.js
@@ -1,29 +1,16 @@
+// Função para alternar a visibilidade de um elemento HTML
 function toggleOcultarExibir() {
-  // Seleciona o elemento com a classe 'area-template-fil-content'
-  var elemento = document.querySelector(".area-template-fil-content");
-  var botao = document.querySelector(".btn-recuar-expandir");
+  // Seleciona o primeiro elemento HTML com a classe .area-template-fil-content
+  var areaContent = document.querySelector(".area-template-fil-content");
 
-  // Verifica se a largura da janela do navegador é menor que 768 pixels
-  if (window.matchMedia("(max-width: 768px)").matches) {
-    // Verifica se o elemento está sendo exibido ou não
-    if (elemento.style.display === "none") {
-      // Se o elemento está oculto, remove a propriedade 'display' para retornar ao valor original da classe
-      elemento.style.removeProperty("display");
-      setTimeout(() => (botao.textContent = "Ocultar pesquisa rápida"), 50);
-    } else {
-      // Se o elemento está sendo exibido, oculta o elemento e altera o texto do botão para 'Exibir'
-      elemento.style.display = "none";
-      setTimeout(() => (botao.textContent = "Exibir pesquisa rápida"), 50);
-    }
+  // Verifica se o elemento tem um valor definido para a propriedade maxHeight de seu estilo
+  if (areaContent.style.maxHeight) {
+    // Se maxHeight estiver definido, redefine para null, removendo a restrição de altura máxima
+    areaContent.style.maxHeight = null;
   } else {
-    // Se a largura da janela do navegador é maior que 768 pixels, remove a propriedade 'display' para retornar ao valor original da classe
-    elemento.style.removeProperty("display");
-    setTimeout(() => (botao.textContent = "Ocultar pesquisa rápida"), 50);
+    // Se maxHeight não estiver definido, define maxHeight para a altura atual do conteúdo do elemento
+    areaContent.style.maxHeight = areaContent.scrollHeight + "px";
   }
+  // Faz a janela do navegador rolar para o topo
+  window.scrollTo(0, 0);
 }
-
-// Adiciona um ouvinte de evento para a mudança de tamanho da janela
-window.addEventListener("resize", toggleOcultarExibir);
-
-// Adiciona um ouvinte de evento para o carregamento da página
-window.addEventListener("load", toggleOcultarExibir);
--- Front/static/JS/RecuarExpandirVertical.js
+++ Front/static/JS/RecuarExpandirVertical.js
@@ -1,5 +1,30 @@
 function toggleRecuarExpandirVertical() {
-  var sidenav = document.querySelector(".recuar-expandir-vertical");
-  sidenav.classList.toggle("recuar");
-  window.scrollTo(0, 0);
+  // Obtenha o elemento
+  var element = document.querySelector(".area-template-tmf-alt-content");
+
+  // Verifique se a largura da tela é menor ou igual a 768px
+  var mediaQuery = window.matchMedia("(max-width: 768px)");
+
+  // Adicione um ouvinte de evento de mudança de mídia
+  mediaQuery.addListener(function (e) {
+    if (!e.matches) {
+      // Se a largura da tela for maior que 768px, recarregue a página
+      location.reload();
+    }
+  });
+
+  if (mediaQuery.matches) {
+    // Obtenha a altura atual do elemento
+    var currentHeight = window.getComputedStyle(element).height;
+
+    // Altere a altura do elemento
+    if (currentHeight === "100px") {
+      element.style.height = "auto";
+    } else {
+      element.style.height = "100px";
+    }
+  } else {
+    // Se a largura da tela for maior que 768px, defina a altura como auto
+    element.style.height = "auto";
+  }
 }
