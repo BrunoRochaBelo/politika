f1dd3ae77157e292f3c9b17cb3ea0751160b14f7
BrunoRochaBelo <bruno.r.belo@hotmail.com>
2024-06-10 01:51:26 -0300

    Construção do site DICA, falta finalizar

---
+++ Site/static/JS/script.js
@@ -0,0 +1,93 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const headers = document.querySelectorAll("#como-potencializa ul li h4");
+  headers.forEach((header) => {
+    header.style.cursor = "pointer";
+    header.addEventListener("click", function () {
+      const detail = this.nextElementSibling;
+      if (detail.style.display === "none" || detail.style.display === "") {
+        detail.style.display = "block";
+      } else {
+        detail.style.display = "none";
+      }
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const recursoItems = document.querySelectorAll(".recurso-item");
+
+  recursoItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      const isActive = item.classList.contains("active");
+
+      // Remover classe active de todos os itens
+      recursoItems.forEach((i) => i.classList.remove("active"));
+
+      // Se o item clicado não estava ativo, ativá-lo
+      if (!isActive) {
+        item.classList.add("active");
+      }
+    });
+
+    item.addEventListener("mouseover", function () {
+      item.classList.add("hover");
+    });
+
+    item.addEventListener("mouseout", function () {
+      item.classList.remove("hover");
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const detailToggle = document.querySelector("#sobre-nos .toggle-details");
+  const detailContent = document.querySelector("#sobre-nos .detail");
+
+  detailToggle.addEventListener("click", function () {
+    detailContent.classList.toggle("visible");
+  });
+});
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+    // Scroll para baixo
+    document.querySelector(".nav-menu").classList.add("hidden");
+  } else {
+    // Scroll para cima
+    document.querySelector(".nav-menu").classList.remove("hidden");
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Para o caso de scroll no topo da página
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon img");
+  const menuList = document.querySelector(".menu-list");
+  const menuItems = document.querySelectorAll(".menu-list li a");
+
+  // Adiciona um ouvinte de evento de clique ao ícone do menu
+  menuIcon.addEventListener("click", function (event) {
+    event.stopPropagation(); // Impede que o clique no ícone do menu propague para o documento
+    menuList.classList.toggle("show-menu");
+  });
+
+  // Adiciona um ouvinte de evento de clique ao documento inteiro
+  document.addEventListener("click", function (event) {
+    // Verifica se o clique ocorreu fora do menu e do ícone do menu
+    if (!menuList.contains(event.target) && !menuIcon.contains(event.target)) {
+      menuList.classList.remove("show-menu"); // Fecha o menu se o clique foi fora do menu e do ícone do menu
+    }
+  });
+
+  // Adiciona um ouvinte de evento de clique a cada item do menu
+  menuItems.forEach(function (menuItem) {
+    menuItem.addEventListener("click", function () {
+      menuList.classList.remove("show-menu"); // Fecha o menu ao clicar em um item do menu
+    });
+  });
+});
