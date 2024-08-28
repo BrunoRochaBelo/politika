232d8de01e8376cd8d2fae2b94243412c5c0fbd1
BrunoRochaBelo <bruno.r.belo@hotmail.com>
2024-08-27 23:11:01 -0300

    Ajustes no botão "recuar-expandir" dos "indicadores e estatisticas"

--- Front/static/JS/RecuarExpandirHorizontal.js
+++ Front/static/JS/RecuarExpandirHorizontal.js
@@ -8,10 +8,12 @@ function toggleRecuarExpandirHorizontal() {
       element.classList.remove("expandir");
       element.style.height = ""; // Volta para auto
     });
+    button.classList.remove("up");
     button.innerHTML = "Expandir";
   } else {
     element.classList.add("expandir");
     element.style.height = ""; // Garante que a altura esteja correta
+    button.classList.add("up");
     button.innerHTML = "Recuar";
   }
 }
