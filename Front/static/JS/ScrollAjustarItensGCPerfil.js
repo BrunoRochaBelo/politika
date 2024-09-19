4bd4b833edeaa418383a92b338e15cd863f3c571
BrunoRochaBelo <bruno.r.belo@hotmail.com>
2024-09-19 13:07:09 -0300

    Criação do "ScrollAjustarItensGCPerfil.js" e ajustes referentes ao Gerenciar Contas - Perfil

---
+++ Front/static/JS/ScrollAjustarItensGCPerfil.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const headerContainer = document.querySelector(
+    ".container-abas-template-header"
+  );
+  const profileImage = document.querySelector(".img-perfil");
+  const navElement = document.querySelector(
+    ".container-abas-template-header .nav"
+  );
+  const cardSessionContent = document.querySelector(
+    ".area-interna-containerContent-template-content"
+  );
+  const mainContainer = document.querySelector(
+    ".main-gc .area-interna-containerContent-template"
+  );
+
+  // Armazenar estilos originais
+  const originalStyles = {
+    profileImageWidth: profileImage ? profileImage.style.width || "" : "",
+    profileImageHeight: profileImage ? profileImage.style.height || "" : "",
+    profileImageMarginBottom: profileImage
+      ? profileImage.style.marginBottom || ""
+      : "",
+    navElementTransform: navElement ? navElement.style.transform || "" : "",
+    mainContainerMaxHeight: mainContainer
+      ? mainContainer.style.maxHeight || ""
+      : "",
+    headerContainerPadding: headerContainer
+      ? headerContainer.style.padding || ""
+      : "",
+  };
+
+  function restoreOriginalStyles() {
+    if (profileImage) {
+      profileImage.style.width = originalStyles.profileImageWidth;
+      profileImage.style.height = originalStyles.profileImageHeight;
+      profileImage.style.marginBottom = originalStyles.profileImageMarginBottom;
+    }
+    if (navElement)
+      navElement.style.transform = originalStyles.navElementTransform;
+    if (mainContainer)
+      mainContainer.style.maxHeight = originalStyles.mainContainerMaxHeight;
+    if (headerContainer)
+      headerContainer.style.padding = originalStyles.headerContainerPadding;
+  }
+
+  function adjustScaleOnScroll() {
+    const screenWidth = window.innerWidth;
+
+    if (this.scrollTop > 0) {
+      if (screenWidth <= 900) {
+        // 56.25rem em px
+        if (profileImage) {
+          profileImage.style.width = "60px";
+          profileImage.style.height = "60px";
+          profileImage.style.marginBottom = "0"; // Remove a margin-bottom
+        }
+        if (navElement) navElement.style.transform = "scale(0.85)";
+        if (mainContainer)
+          mainContainer.style.maxHeight =
+            "calc(var(--vh, 1vh) * 100 - 11.285rem) !important";
+        if (headerContainer) headerContainer.style.padding = "3px";
+      } else {
+        // Para telas maiores
+        if (profileImage) {
+          profileImage.style.width = "80px";
+          profileImage.style.height = "80px";
+          profileImage.style.marginBottom = "0"; // Remove a margin-bottom
+        }
+        if (navElement) navElement.style.transform = "scale(0.9)";
+        if (mainContainer)
+          mainContainer.style.maxHeight =
+            "calc(var(--vh, 1vh) * 100 - 12.85rem) !important";
+      }
+    } else {
+      restoreOriginalStyles();
+    }
+  }
+
+  function handleScroll() {
+    requestAnimationFrame(adjustScaleOnScroll.bind(this));
+  }
+
+  if (cardSessionContent) {
+    cardSessionContent.addEventListener("scroll", handleScroll);
+  }
+});
